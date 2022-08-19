5f3873102f2ed0efe19be2f98a7870deb650abad
Robert Wiebe <robert.wiebe9@icloud.com>
2022-08-19 16:06:45 +0200

    Initial Commit

---
+++ Stock Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock Game
+//
+//  Created by Robert Wiebe on 8/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stock Game/Stock_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_GameApp.swift
+//  Stock Game
+//
+//  Created by Robert Wiebe on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Stock_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
